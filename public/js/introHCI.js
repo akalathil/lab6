2ffd9db0ae9293f979d7bfe215e2f8922c4f8641
Arvind Kalathil <akalathi@ucsd.edu>
2016-02-11 11:31:40 -0800

    fixed get requests url

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,7 +27,7 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	$.get('http://localhost:3000/project/' + idNumber, addDetails);
+	$.get('/project/' + idNumber, addDetails);
 	console.log("User clicked on project " + idNumber);
 
 }
