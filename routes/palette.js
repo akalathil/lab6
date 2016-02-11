e238fb496ddaabbca7b065df8d27209484d6fa0b
Arvind Kalathil <akalathi@ucsd.edu>
2016-02-11 11:38:31 -0800

    changed response of /palette'

--- routes/palette.js
+++ routes/palette.js
@@ -2,6 +2,6 @@ var palettes = require('../palettes.json');
 
 exports.randomPalette = function(req, res) {
 	// get a random palette from the top ones
-	var randomPalette = palettes[Math.floor(palettes.length * Math.random())];
-	res.json('Your random palette is called: ' + randomPalette['title'], randomPalette);
+	var colors = palettes[Math.floor(palettes.length * Math.random())];
+	res.json(colors);
 }
